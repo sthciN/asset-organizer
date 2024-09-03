cb4b76397839f56ad3aa77d8424579430802df77
atefeh <atefeh.barzideh@hotmail.com>
2024-09-03 13:52:19 +0200

    Allow origin from frontend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from services.sql_app import crud, models, schemas
+from fastapi.middleware.cors import CORSMiddleware
 from services.sql_app.database import SessionLocal, engine
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -9,3 +10,11 @@ load_dotenv()
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Adjust to your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
